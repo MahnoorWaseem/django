e38d888bdd54fbf01bfa24f85ade22bfbaf74199
abdulmoizazher <abdulmoizazher@gmail.com>
2025-04-29 00:40:03 +0500

    Add with_perm method to retrieve users with a specific permission

--- django/contrib/auth/backends.py
+++ django/contrib/auth/backends.py
@@ -206,3 +206,35 @@ from django.contrib.auth.models import Permission
         except UserModel.DoesNotExist:
             return None
         return user if self.user_can_authenticate(user) else None
+
+from django.db.models import Exists, OuterRef, Q
+
+    def with_perm(self, perm, is_active=True, include_superusers=True, obj=None):
+        if isinstance(perm, str):
+            try:
+                app_label, codename = perm.split(".")
+            except ValueError:
+                raise ValueError(
+                    "Permission name should be in the form app_label.permission_codename."
+                )
+        elif not isinstance(perm, Permission):
+            raise TypeError(
+                "The `perm` argument must be a string or a permission instance."
+            )
+
+        if obj is not None:
+            return UserModel._default_manager.none()
+
+        permission_q = Q(group__user=OuterRef("pk")) | Q(user=OuterRef("pk"))
+        if isinstance(perm, Permission):
+            permission_q &= Q(pk=perm.pk)
+        else:
+            permission_q &= Q(codename=codename, content_type__app_label=app_label)
+
+        user_q = Exists(Permission.objects.filter(permission_q))
+        if include_superusers:
+            user_q |= Q(is_superuser=True)
+        if is_active is not None:
+            user_q &= Q(is_active=is_active)
+
+        return UserModel._default_manager.filter(user_q)
