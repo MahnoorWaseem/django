b374a82a2a08dbee7fe816d28021bf54fc1a22aa
Zohaib Ali <zohaib.ali236@gmail.com>
2025-04-29 00:49:27 +0500

    Add other fields

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -1692,3 +1692,111 @@ class SplitDateTimeField(MultiValueField):
             result = datetime.datetime.combine(*data_list)
             return from_current_timezone(result)
         return None
+
+class SlugField(CharField):
+    default_validators = [validators.validate_slug]
+
+    def __init__(self, *, allow_unicode=False, **kwargs):
+        self.allow_unicode = allow_unicode
+        if self.allow_unicode:
+            self.default_validators = [validators.validate_unicode_slug]
+        super().__init__(**kwargs)
+
+class UUIDField(CharField):
+    default_error_messages = {
+        "invalid": _("Enter a valid UUID."),
+        "required": _("This field is required."),
+    }
+
+    def prepare_value(self, value):
+        if isinstance(value, uuid.UUID):
+            return str(value)
+        return value
+
+    def to_python(self, value):
+        value = super().to_python(value)
+        if value in self.empty_values:
+            return None
+        if not isinstance(value, uuid.UUID):
+            try:
+                value = uuid.UUID(value)
+            except ValueError:
+                raise ValidationError(self.error_messages["invalid"], code="invalid")
+        return value
+
+class InvalidJSONInput(str):
+    pass
+
+class JSONString(str):
+    pass
+
+class JSONField(CharField):
+    default_error_messages = {
+        "invalid": _("Enter a valid JSON."),
+        "required": _("This field is required."),
+    }
+    widget = Textarea
+
+    def __init__(self, encoder=None, decoder=None, **kwargs):
+        self.encoder = encoder
+        self.decoder = decoder
+        super().__init__(**kwargs)
+
+    def to_python(self, value):
+        if self.disabled:
+            return value
+        if value in self.empty_values:
+            return None
+        elif isinstance(value, (list, dict, int, float, JSONString)):
+            return value
+        try:
+            converted = json.loads(value, cls=self.decoder)
+        except json.JSONDecodeError:
+            raise ValidationError(
+                self.error_messages["invalid"],
+                code="invalid",
+                params={"value": value},
+            )
+        if isinstance(converted, str):
+            return JSONString(converted)
+        else:
+            return converted
+
+    def bound_data(self, data, initial):
+        if self.disabled:
+            return initial
+        if data is None:
+            return None
+        try:
+            return json.loads(data, cls=self.decoder)
+        except json.JSONDecodeError:
+            return InvalidJSONInput(data)
+
+    def prepare_value(self, value):
+        if isinstance(value, InvalidJSONInput):
+            return value
+        return json.dumps(value, ensure_ascii=False, cls=self.encoder)
+
+    def has_changed(self, initial, data):
+        if super().has_changed(initial, data):
+            return True
+        return json.dumps(initial, sort_keys=True, cls=self.encoder) != json.dumps(
+            self.to_python(data), sort_keys=True, cls=self.encoder
+        )
+    
+    
+class GenericIPAddressField(CharField):
+    def __init__(self, *, protocol="both", unpack_ipv4=False, **kwargs):
+        self.unpack_ipv4 = unpack_ipv4
+        self.default_validators = validators.ip_address_validators(protocol, unpack_ipv4)
+        kwargs.setdefault("max_length", MAX_IPV6_ADDRESS_LENGTH)
+        super().__init__(**kwargs)
+
+    def to_python(self, value):
+        if value in self.empty_values:
+            return ""
+        value = value.strip()
+        if value and ":" in value:
+            return clean_ipv6_address(value, self.unpack_ipv4, max_length=self.max_length)
+        return value
+
